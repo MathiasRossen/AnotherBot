64eaec45adb888cc548012f9716b16bf11f9d141
mathiasrossen <mathias.rossen@gmail.com>
2018-09-15 18:05:27 +0200

    Initial commit

---
+++ CustomKitchenDeliveries/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace CustomKitchenDeliveries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task MainAsync()
+        {
+            DiscordSocketClient client = new DiscordSocketClient();
+
+            // Makes sure the thread never dies
+            await Task.Delay(-1);
+        }
+    }
+}
